4567794a2c4edfc3d06ac11b0798f566db3a0fce
Genesis Thomas <gthomas@perforce.com>
2021-01-14 17:36:42 +0530

    Junit 5 Test watcher sample

---
+++ README.md
@@ -0,0 +1,76 @@
+### Sample Junit 5 + Test watcher project
+
+![CircleCI status](https://circleci.com/gh/PerfectoMobileSA/Junit5TestWatcherSample.svg?style=shield "CircleCI status")
+
+This sample project is designed to get you up and running within few simple steps.
+
+Begin with installing the dependencies below, and continue with the Getting Started procedure below.
+
+### Dependencies
+There are several prerequisite dependencies you should install on your machine prior to starting to work with this project:
+
+* [Java 8](http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
+
+* An IDE to write your tests on - [Eclipse](http://www.eclipse.org/downloads/packages/eclipse-ide-java-developers/marsr) or [IntelliJ](https://www.jetbrains.com/idea/download/#)
+
+* [Maven](https://maven.apache.org/)
+
+
+Eclipse users should also install:
+
+1. [Maven Plugin](http://marketplace.eclipse.org/content/m2e-connector-maven-dependency-plugin)
+
+
+IntelliJ IDEA users should also install:
+
+1. [Maven Plugin for IDEA](https://plugins.jetbrains.com/plugin/1166)
+
+
+## Downloading the Sample Project
+
+* Clone this repository.
+
+* After downloading and unzipping the project to your computer, open it from your IDE by choosing the folder containing the pom.xml 
+
+**********************
+
+
+## Running sample as is
+
+
+* Open TestBase.java </p>
+
+* Search for the below line and replace `<<cloud name>>` with your perfecto cloud name (e.g. demo) or pass it as maven properties: `-DcloudName=<<cloud name>>`</br>  
+		&nbsp;&nbsp;	&nbsp;&nbsp; String cloudName = `"<<cloud name>>"`;
+		</br>
+		</p>
+* Search for the below line and replace `<<SECURITY TOKEN>>` with your perfecto [security token](https://developers.perfectomobile.com/display/PD/Generate+security+tokens) or pass it as maven properties: `-DsecurityToken=<<SECURITY TOKEN>>` </br></p>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; String securityToken = `"<<SECURITY TOKEN>>"`;
+	</br>
+
+Note: Refer to official documentation on how to execute from eclipse / IntelliJ. </br>
+* Run pom.xml with the below maven goals & properties when: </p>
+   a. If credentials are hardcoded:
+		
+		clean
+		install
+		
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;b. If credentials are passed as parameters:
+		
+		clean
+		install
+		-DcloudName=${cloudName}
+		-DsecurityToken=${securityToken}
+</p>
+
+* CI dashboard integration can be performed by supplying the below parameters to top-level Maven Targets:
+
+		clean
+		install
+		-DcloudName=${cloudName}
+		-DtestngXmlFile=testng_perfecto.xml
+		-DsecurityToken=${securityToken}
+		-Dreportium-job-name=${JOB_NAME} 
+		-Dreportium-job-number=${BUILD_NUMBER} 
+		-Dreportium-job-branch=${GIT_BRANCH} 
+		-Dreportium-tags=${myTag}
---
+++ src/test/java/perfecto/TestBase.java
@@ -0,0 +1,94 @@
+package perfecto;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.rules.TestName;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.perfecto.reportium.client.ReportiumClient;
+import com.perfecto.reportium.test.TestContext;
+import org.junit.jupiter.api.*;
+import io.appium.java_client.AppiumDriver;
+
+@ExtendWith(PerfectoWatcher.class)
+public class TestBase {
+	public static RemoteWebDriver driver;
+	public static ReportiumClient reportiumClient;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		// Replace <<cloud name>> with your perfecto cloud name (e.g. demo) or pass it as maven properties: -DcloudName=<<cloud name>>
+		String cloudName = "<<cloud name>>";
+		// Replace <<security token>> with your perfecto security token or pass it as  maven properties: -DsecurityToken=<<SECURITY TOKEN>> More info:
+		// https://developers.perfectomobile.com/display/PD/Generate+security+tokens
+		String securityToken = "<<security token>>";
+
+		
+		// A sample perfecto connect appium script to connect with a perfecto android
+		// device and perform addition validation in calculator app.
+		String browserName = "mobileOS";
+		DesiredCapabilities capabilities = new DesiredCapabilities(browserName, "", Platform.ANY);
+		capabilities.setCapability("securityToken", Utils.fetchSecurityToken(securityToken));
+		capabilities.setCapability("model", "Galaxy S.*");
+		capabilities.setCapability("enableAppiumBehavior", true);
+		capabilities.setCapability("openDeviceTimeout", 2);
+		capabilities.setCapability("appPackage", "com.android.settings");
+		capabilities.setCapability("appActivity", "com.android.settings.Settings");
+		try {
+			driver = (RemoteWebDriver) (new AppiumDriver<>(new URL("https://" + Utils.fetchCloudName(cloudName)
+					+ ".perfectomobile.com/nexperience/perfectomobile/wd/hub"), capabilities));
+			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		} catch (SessionNotCreatedException e) {
+			throw new RuntimeException("Driver not created with capabilities: " + capabilities.toString());
+		}
+		reportiumClient = Utils.setReportiumClient(driver, reportiumClient); // Creates reportiumClient
+	}
+	
+	@Test
+	void settingsTest() {
+		reportiumClient.testStart("Junit Watcher Settings Test", new TestContext("tag2", "tag3")); // Starts the reportium test
+
+		reportiumClient.stepStart("Verify Settings App is loaded"); // Starts a reportium step
+		driver.findElement(By.xpath(
+				"//*[contains(@resource-id,':id/collpasing_app_bar_extended_title') or contains(@resource-id,'settings:id/search')] | //*[contains(@text,'Search')]"))
+				.isDisplayed();
+		reportiumClient.stepEnd(); // Stops a reportium step
+
+		reportiumClient.stepStart("Verify Data usage validation");
+		driver.findElement(By.xpath("//*[contains(@text,'Data usage')]")).click();
+		WebElement data = driver
+				.findElement(By.xpath("//*[contains(@resource-id, 'action_bar')]//*[@text='Data usage']"));
+		Utils.assertText(data, reportiumClient, "Data usage");
+		reportiumClient.stepEnd();
+
+	}
+	
+	@Test
+	@Disabled
+	void failTest() {
+		// Fail test wantedly ( currently disabled ) 
+		reportiumClient.testStart("Junit Watcher Fail Settings Test", new TestContext("fail", "tag3")); 
+		reportiumClient.stepStart("Verify Settings App is loaded"); 
+		driver.findElement(By.xpath(
+				"//*[contains(@resource-id,':id/1111search')]"))
+				.isDisplayed();
+		reportiumClient.stepEnd(); 
+	}
+	
+	public RemoteWebDriver getDriver() {
+        return driver;
+    }
+	
+	public ReportiumClient getReportiumClient() {
+        return reportiumClient;
+    }
+
+}
